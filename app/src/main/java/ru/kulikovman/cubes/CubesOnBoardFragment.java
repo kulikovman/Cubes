78083f5508b1357475d2ee351c9e91e0f0c08120
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-01-06 10:55:52 +0500

    Добавлены фрагменты и навигационный граф

---
+++ app/src/main/java/ru/kulikovman/cubes/CubesOnBoardFragment.java
@@ -0,0 +1,39 @@
+package ru.kulikovman.cubes;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.kulikovman.cubes.databinding.FragmentCubesOnBoardBinding;
+
+
+public class CubesOnBoardFragment extends Fragment {
+
+    private FragmentCubesOnBoardBinding binding;
+
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cubes_on_board, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
---
+++ app/src/main/java/ru/kulikovman/cubes/SettingFragment.java
@@ -0,0 +1,39 @@
+package ru.kulikovman.cubes;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.kulikovman.cubes.databinding.FragmentSettingBinding;
+
+
+public class SettingFragment extends Fragment {
+
+    private FragmentSettingBinding binding;
+
+    private Context context;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_setting, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+}
